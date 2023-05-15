8d64dba13aaae7687c878bef2a190bf0449a35b8
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-05-15 22:17:02 +0600

    Update impulse function.m

--- impulse function.m
+++ impulse function.m
@@ -5,6 +5,8 @@ n2 = 10;
 n = 0;
 x = n1:n2;
 y = (x-n==0);
+% y = (x-n>=0) for step function
+% y = x.*(x-n>=0) for ramp function_ 
 stem(x,y,'r','LineWidth',5);
 xlabel('\bf\color{blue}\fontsize{30}Sample')
 ylabel('\bf\color{blue}\fontsize{30}Amplitude');
