b8d9e88b635df35de969c2a0dac49ad5b68d4dce
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-05-20 01:08:09 +0600

    Update sampling.m

--- sampling.m
+++ sampling.m
@@ -7,10 +7,20 @@ f = 1;
 x = sin(2*pi*f*t);
 plot(t,x);
 
-fs = 10;
+fs = 5;
 n = 0:2*fs-1;
 
-y = sin(2*pi*f*n/fs);
+y = 5*sin(2*pi*f*n/fs);
 figure();
 stem(n,y);
+y = round(y);
+yy = abs(y);
+s1 =dec2bin(yy,3);
+s1 = string(s1);
 
+for i = 1:10
+    if(y(1,i)<0)
+        y(1,i) = '1';
+    end
+end
+s1
