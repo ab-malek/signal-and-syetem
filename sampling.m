ad54e749aef3cc4d7c862162875b156ec2bac766
Abdul Malek <109610531+ab-malek@users.noreply.github.com>
2023-05-21 03:07:46 +0600

    Update sampling.m

--- sampling.m
+++ sampling.m
@@ -24,3 +24,40 @@ for i = 1:10
     end
 end
 s1
+
+
+
+
+
+
+New 
+
+
+clc;
+clear all;
+close all;
+h=3;
+d=(10-5)/3;
+f=1/d;
+x=0:0.001:2;
+y=5*sin(2*pi*1*x);
+subplot(2,1,1);
+plot(x,y);
+n=0:2/20:2;
+g=5*sin(2*pi*1*n);
+subplot(2,1,2);
+stem(n,g);
+v=round(g);
+d=abs(v);
+r=dec2bin(d,4);
+for i=1:length(r)
+    if v(i)<0
+       r(i,1)='1';
+    end
+end
+s1=string(r);
+s=s1(1);
+for i=2:length(s1)
+    s=s+s1(i);
+end
+s
